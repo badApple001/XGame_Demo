de7171c948297909eae169292f3df96fc2350556
badApple001 <isysprey@foxmail.com>
2025-05-20 18:28:20 +0800

    本地数据 + 自动扫描标记脏数据保存

---
+++ Assets/Game/HeroTeam/GameResources/Scripts/Data/HeroTeamLocalData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XGameEngine.Player;
+
+namespace GameScripts.HeroTeam
+{
+
+    public class HeroTeamLocalData : PlayerSerializableData
+    {
+        /// <summary>
+        /// 最后一次登录游戏的时间戳
+        /// </summary>
+        public long LastLoginTimestamps;
+
+
+
+    }
+}
